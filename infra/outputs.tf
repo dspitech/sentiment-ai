38fab6560a2f32ebafa98202082abc3227019d3e
Pape Lo <pape.lo@estiam.com>
2026-06-19 17:14:42 +0000

    Update

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,16 +1,33 @@
+
 # infra/outputs.tf
 
+
+
 output "container_id" {
+
   description = "ID du conteneur staging"
+
   value       = docker_container.sentiment_staging.id
+
 }
+
+
 
 output "app_url" {
+
   description = "URL de l'application staging"
+
   value       = "http://localhost:${var.app_port}"
+
 }
 
+
+
 output "network_name" {
-  description = "Nom du réseau Docker créé"
-  value       = docker_network.cicd.name
+
+  description = "Nom du réseau Docker utilisé"
+
+  value       = data.docker_network.cicd.name
+
 }
+
